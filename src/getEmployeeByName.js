a885070c88e1b130708f7e9b7f7b8ed697e6fb92
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-01 16:14:06 -0300

    added getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName) || {};
 }
+
+console.log(getEmployeeByName('Wishart'));
 
 module.exports = getEmployeeByName;
