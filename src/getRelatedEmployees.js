ddc3e2a04dd0e327ff54c311f6100b99858d3118
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-01 19:22:48 -0300

    added getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
+const managers = [
+  '9e7d4524-363c-416a-8759-8aa7e50c0992',
+  'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+  '0e7b460e-acf4-4e17-bcb3-ee472265db83'
+]
 
 function isManager(id) {
-  // seu código aqui
+  return managers.includes(id)
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  let listEmployees = []
+  if (isManager(managerId)) {
+    data.employees.forEach((employee) =>{
+      if (employee.managers.includes(managerId)) {
+        listEmployees.push(`${employee.firstName} ${employee.lastName}`)
+      }
+    })
+    return listEmployees
+  }
+  throw 'O id inserido não é de uma pessoa colaboradora gerente!'
+  
 }
 
 module.exports = { isManager, getRelatedEmployees };
