596f16b13c42c2542519f2f74d42367eba26eaad
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-05 14:29:48 -0300

    added calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
+function somaEntrants(entrants) {
+  let some = 0;
+  some += data.prices.adult * entrants.adult || 0;
+  some += data.prices.child * entrants.child || 0;
+  some += data.prices.senior * entrants.senior || 0;
+  return some;
+}
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((people) => people.age < 18);
+  const adult = entrants.filter((people) => people.age >= 18 && people.age < 50);
+  const senior = entrants.filter((people) => people.age >= 50);
+  return {
+    adult: adult.length,
+    child: child.length,
+    senior: senior.length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === undefined) {
+    return 0;
+  }
+  const countEntrant = countEntrants(entrants);
+  return somaEntrants(countEntrant);
 }
 
 module.exports = { calculateEntry, countEntrants };
