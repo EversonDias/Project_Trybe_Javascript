106b3e9ebd13ea0dcb8ba250121d1af9440e2dcc
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-07 15:39:02 -0300

    added getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const wantedSpecies = (props) => {
+  const infoAnimal = {
+    species: [],
+    locations: [],
+  };
+  const species = props.map((id) => data.species.find((animal) => animal.id === id));
+  species.forEach((animal) => {
+    infoAnimal.species.push(animal.name);
+    infoAnimal.locations.push(animal.location);
+  });
+  return infoAnimal;
+};
+
+const createInfo = (props) => {
+  const infoSpecies = wantedSpecies(props.responsibleFor);
+  return ({
+    id: props.id,
+    fullName: `${props.firstName} ${props.lastName}`,
+    species: infoSpecies.species,
+    locations: infoSpecies.locations,
+  });
+};
+
+function getEmployeesCoverage(props) {
+  try {
+    if (!props) {
+      const listEmployee = data.employees.map((people) => createInfo(people));
+      return listEmployee;
+    }
+    if (props.name) {
+      const employees = data.employees.find((people) =>
+        people.firstName === props.name
+        || people.lastName === props.name);
+      return createInfo(employees);
+    }
+    if (props.id) {
+      const employees = data.employees.find((people) => people.id === props.id);
+      return createInfo(employees);
+    }
+  } catch (error) {
+    throw new Error('Informações inválidas');
+  }
 }
 
 module.exports = getEmployeesCoverage;
