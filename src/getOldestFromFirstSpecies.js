b81a16fb02e6aa66ec7ab218c2a684c046bc900f
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-07 13:26:44 -0300

    added getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let ageAnimal = { age: 0 };
+  const employeeSelected = data.employees.find((people) => people.id === id);
+  const idSpecieResponsible = employeeSelected.responsibleFor[0];
+  const oldSpecie = data.species.find((animal) => animal.id === idSpecieResponsible).residents;
+  oldSpecie.forEach((animal) => {
+    if (animal.age > ageAnimal.age) {
+      ageAnimal = animal;
+    }
+  });
+  return Object.values(ageAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
